dbb8b26bf3533cab64d6a2e161d020b87388b364
Gianni Mariani <owebeeone@gmail.com>
2025-08-31 11:56:14 +1000

    Fixed hangs on large files - added threshold for syntax highlighting

--- src/components/a_ui/CodeViewer.tsx
+++ src/components/a_ui/CodeViewer.tsx
@@ -1,14 +1,24 @@
 import { useGrip } from '@owebeeone/grip-react';
 import { CURRENT_CODE_TEXT } from '../../grips';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 export default function CodeViewer() {
-    const code = useGrip(CURRENT_CODE_TEXT);
+  const code = useGrip(CURRENT_CODE_TEXT);
+  const MAX_HILITE = 30_000; // ~300KB threshold for highlighting
+  const canHighlight = typeof code === 'string' && code.length > 0 && code.length <= MAX_HILITE;
 
-    return (
-        <div className="h-full w-full overflow-auto">
-            <pre className="m-0 p-2 text-gray-200 whitespace-pre-wrap">
-                {code ?? '// Loading...'}
-            </pre>
-        </div>
-    );
+  return (
+    <div className="h-full w-full overflow-auto">
+      {canHighlight ? (
+        <SyntaxHighlighter language="scss" style={dracula} customStyle={{ margin: 0, height: '100%' }}>
+          {code}
+        </SyntaxHighlighter>
+      ) : (
+        <pre className="m-0 p-2 text-gray-200 whitespace-pre-wrap">
+          {code ?? '// Loading...'}
+        </pre>
+      )}
+    </div>
+  );
 }
